372317c29a58cecb08a6cefcbd1a11ccd9fcee10
Magikarp <magikarp@karp.karp>
2014-03-13 15:02:27 -0400

    added prelim data struct for node tracking data

---
+++ nodeDataStruct.py
@@ -0,0 +1,13 @@
+
+
+class NodeTrackingData:
+    int servoPanDegree = 0
+    int servoTiltDegree = 0
+    int wifiStrength = 0
+    int GPS_epoch = 0
+    int wifiPoll_epoch = 0
+    float GPS_Longitude_degree = 0
+    float GPS_Longitude_minute = 0
+    float GPS_Latitude_degree = 0
+    float GPS_Latitude_minute = 0
+    float Altitude_cm = 0
