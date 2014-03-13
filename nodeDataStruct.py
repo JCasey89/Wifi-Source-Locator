98f23f01484c50e188d61ef7aa21f9e1fef9d0e7
Magikarp <magikarp@karp.karp>
2014-03-13 15:22:13 -0400

    re-ordered time variables in data struct

--- nodeDataStruct.py
+++ nodeDataStruct.py
@@ -4,8 +4,8 @@ class NodeTrackingData:
     int servoPanDegree = 0
     int servoTiltDegree = 0
     int wifiStrength = 0
-    int GPS_epoch = 0
     int wifiPoll_epoch = 0
+    int GPS_epoch = 0
     float GPS_Longitude_degree = 0
     float GPS_Longitude_minute = 0
     float GPS_Latitude_degree = 0
