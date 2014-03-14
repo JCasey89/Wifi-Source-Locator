c6b66566f334af8b2432c28314af08a9a73a4acf
Magikarp <magikarp@karp.karp>
2014-03-13 20:25:46 -0400

    changed Node to an object, added wrapper functions for reading signal strength and servo movement

--- node.py
+++ node.py
@@ -1,13 +1,53 @@
+import time
+import antenna
+#import servo
 
+class Node(object):
+    strInterfaceName = ""
+    intServoPanDegree = 0 #Final
+    intServoTiltDegree = 0 #Final
+    intWifiStrength = 0
+    intWifiPoll_epoch = 0
+    intGPS_epoch = 0
+    floatGPS_Longitude_degree = 0.0
+    floatGPS_Longitude_minute = 0.0
+    floatGPS_Latitude_degree = 0.0
+    floatGPS_Latitude_minute = 0.0
+    floatAltitude_cm = 0.0
+    
+    def __init__(self, strName):
+        self.strInterfaceName = str(strName)
 
-class NodeTrackingData:
-    int servoPanDegree = 0
-    int servoTiltDegree = 0
-    int wifiStrength = 0
-    int wifiPoll_epoch = 0
-    int GPS_epoch = 0
-    float GPS_Longitude_degree = 0
-    float GPS_Longitude_minute = 0
-    float GPS_Latitude_degree = 0
-    float GPS_Latitude_minute = 0
-    float Altitude_cm = 0
+    #wrapper to get signal strength
+    #returns -db
+    def get_strength_connected(self):
+        return (int(antenna.get_strength_connected(self.strInterfaceName) * -1))
+
+    def get_average_strength_connected(self):
+        intTotalStrengths = 0
+        
+        for i in range(5):#1 second debounce
+            intTotalStrengths += self.get_strength_connected()
+            time.sleep(0.2)
+        intAverageStrength = int(intTotalStrengths/5)
+        return intAverageStrength
+
+    #wrapper to move the servo
+    #inputs
+    #   strDirection: "pan" or "tilt"
+    #   intDegrees: degree to move to
+    #outputs:
+    #   boolSuccess
+    def move(self, strDirection, intDegree):
+        input("Debug: %r antenna to %r degree then hit enter"\
+                 % (strDirection, intDegree))
+        return
+
+#    def pan(intDegree):
+#        raw_input("Debug: Move antenna to %r degree then hit enter"\
+#                % intDegree)
+#        return
+#    def tilt(intDegree):
+#        raw_input("Debug: Move antenna to %r degree then hit enter"\
+#                % intDegree)
+#        return
