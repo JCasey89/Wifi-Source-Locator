e299c02751ea269cc44563c23d0373a453ebc491
Magikarp <magikarp@karp.karp>
2014-03-13 20:23:09 -0400

    renamed wifi_signal_strength_connected to get_strength_connected, included param for selecting wifi cards, now returns and int of the strength

--- antenna.py
+++ antenna.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import subprocess
 import re
@@ -6,17 +5,18 @@ import re
 # regex for 1 or more digit
 number_pattern = re.compile("[0-9]+")
 
-class antenna
-    """Poll wifi card for signal strength"""
 
-    #Poll signal strength when connected to AP
-    def wifi_strength_connected():
-    # read the relevant line of /proc/net/wireless for wireless strength
-        file_read_process = subprocess.Popen("cat /proc/net/wireless | grep -i wlp1s0",\
-                shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        file_line = file_read_process.stdout.readline()
+#Poll signal strength when connected to AP
+# returns -db stripped of all non-digit symbols
+def get_strength_connected(strInterface):
+# read the relevant line of /proc/net/wireless for wireless strength
+    file_read_process = subprocess.Popen("cat /proc/net/wireless | grep -i %r" %strInterface,\
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    file_line = file_read_process.stdout.readline()
 
-        file_line = file_line.split()
-    # pull out the digits from the signal level value
-        signal_strength = number_pattern.search(str(file_line[3]))
-        print(signal_strength.group())
+    file_line = file_line.split()
+# pull out the digits from the signal level value
+    signal_strength = number_pattern.search(str(file_line[3]))
+    sig_str = signal_strength.group()
+#    print(sig_str)
+    return int(sig_str)
