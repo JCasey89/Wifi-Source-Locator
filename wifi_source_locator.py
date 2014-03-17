5d774e40800f2764334e0b793c6586f15837c5d8
Magikarp <magikarp@karp.karp>
2014-03-17 14:49:42 -0400

    added selector for if the user is connected to the target or not

--- wifi_source_locator.py
+++ wifi_source_locator.py
@@ -7,7 +7,11 @@ strInterface = input("Please enter the name of the wireless interface: ")
 
 tracker = node.Node(strInterface)
 
-print(tracker.strInterfaceName)
-intHope = search.binarySearch(tracker, "pan")
-intPray = tracker.get_strength_connected()
-print("%d db at %d degrees" % (intPray, intHope))
+mode = input("Please enter 1 if you are connected to the target AP, otherwise\n\
+        enter 0 if you are not connected to the target AP: ")
+if (int(mode) == 1):
+    intHope = search.binarySearch(tracker, "pan")
+    intPray = tracker.get_strength_connected()
+    print("%d db at %d degrees" % (intPray, intHope))
+else:
+    tracker.select_target()
