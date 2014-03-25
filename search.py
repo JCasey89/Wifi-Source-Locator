08fdf01a79a1a25098fc94986c40348a7ae03b29
Magikarp <magikarp@karp.karp>
2014-03-24 22:53:15 -0400

    search loop seperated into new function

--- search.py
+++ search.py
@@ -1,7 +1,50 @@
 
 import node
 
-def binarySearch(nodeTracker, strDirection):
+
+def Search_Step(nodeTracker, intWorkingRange, strDirection, \
+        intLowerReadPosition, intHigherReadPosition, boolLower):
+    #binary search based off signal strengths
+    if (intLowerReadStrength > intHigherReadStrength):
+        intHigherReadPosition = int(intHigherReadPosition - intWorkingRange)
+        boolLower = True
+        print("->")
+
+    elif (intHigherReadStrength > intLowerReadStrength):
+        intLowerReadPosition = int(intLowerReadPosition + intWorkingRange)
+        print("<-")
+        boolLower = False
+    elif (boolLower):
+        intWorkingRange =- 1
+        intLowerReadPosition = intLowerReadPosition - 2
+        nodeTracker.Move(strDirection, intLowerReadPosition)
+        intLowerReadStrength = nodeTracker.get_average_strength_connected()
+        continue
+    elif (not(boolLower)):
+        intWorkingRange =- 1
+        intHigherReadPosition = intHigherReadPosition - 2
+        nodeTracker.Move(strDirection, intHigherReadPosition)
+        intHigherReadStrength = nodeTracker.get_average_strength_connected()
+        continue
+    else:
+        print("Something bad happened during the search")
+        return -1
+    if (boolLower):
+        nodeTracker.Move(strDirection, intHigherReadPosition)
+        intHigherReadStrength = nodeTracker.get_average_strength_connected()
+    else:
+        nodeTracker.Move(strDirection, intLowerReadPosition)
+        intLowerReadStrength = nodeTracker.get_average_strength_connected()
+    intWorkingRange = intWorkingRange/2
+    
+    return (nodeTracker, intWorkingRange, strDirection, \
+            intLowerReadPosition, intHigherReadPosition, boolLower)
+
+
+
+
+
+def Search(nodeTracker, strDirection):
     intHigherReadPosition = 180
     intLowerReadPosition = 0
     intWorkingRange = intHigherReadPosition - intLowerReadPosition
@@ -17,46 +60,22 @@ def binarySearch(nodeTracker, strDirection):
     intLowerReadStrength = nodeTracker.get_average_strength_connected()
     nodeTracker.move(strDirection, intHigherReadPosition) #not implimented yet
     intHigherReadStrength = nodeTracker.get_average_strength_connected()
-    boolLeft = False
+    boolLower = False
     intWorkingRange = intWorkingRange/2
     while(boolLooking):
         
         if (intWorkingRange<2):
             boolLooking = False
             break
+        step = Search_Step(nodeTracker, intWorkingRange, strDirection, \
+                intLowerReadPosition, intHigherReadPosition, boolLower)
 
-        #binary search based off signal strengths
-        if (intLowerReadStrength > intHigherReadStrength):
-            intHigherReadPosition = int(intHigherReadPosition - intWorkingRange)
-            boolLeft = True
-            print("->")
+        nodeTracker = step[0]
+        intWorkingRange = step[1]
+        intLowerReadPosition = step[3]
+        intHigherReadPosition = step[4]
+        boolLower = step[5]
 
-        elif (intHigherReadStrength > intLowerReadStrength):
-            intLowerReadPosition = int(intLowerReadPosition + intWorkingRange)
-            print("<-")
-            boolLeft = False
-        elif (boolLeft):
-            intWorkingRange =- 1
-            intLowerReadPosition = intLowerReadPosition - 2
-            nodeTracker.move(strDirection, intLowerReadPosition)
-            intLowerReadStrength = nodeTracker.get_average_strength_connected()
-            continue
-        elif (not(boolLeft)):
-            intWorkingRange =- 1
-            intHigherReadPosition = intHigherReadPosition - 2
-            nodeTracker.move(strDirection, intHigherReadPosition)
-            intHigherReadStrength = nodeTracker.get_average_strength_connected()
-            continue
-        else:
-            print("Something bad happened during the search")
-            return -1
-        if (boolLeft):
-            nodeTracker.move(strDirection, intHigherReadPosition)
-            intHigherReadStrength = nodeTracker.get_average_strength_connected()
-        else:
-            nodeTracker.move(strDirection, intLowerReadPosition)
-            intLowerReadStrength = nodeTracker.get_average_strength_connected()
-        intWorkingRange = intWorkingRange/2
 
     #return the half-way point of the final angles
     intFinalDegree = int(((intHigherReadPosition-intLowerReadPosition)/2+ \
