4e21cb0d9a740c499381982137a332967841c5a1
Magikarp <magikarp@karp.karp>
2014-04-10 15:47:30 -0400

    added base for triangulation functions, to be done server-side

---
+++ triangle.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env Python3
+
+
+def distance(node1, node2):
+    intDistance = 0
+
+    return intDistance
+
+def triangulate(node1, node2):
+    targetCoord = ""
+
+    return targetCoord
