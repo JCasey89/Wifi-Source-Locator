404d7d1320b11d656ede19b0a1e2268b72f0aebb
Magikarp <magikarp@karp.karp>
2014-04-10 14:56:37 -0400

    altered to test Search2()

--- wifi_source_locator.py
+++ wifi_source_locator.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import node
 import search
@@ -10,8 +10,12 @@ tracker = node.Node(strInterface)
 mode = input("Please enter 1 if you are connected to the target AP, otherwise\n\
         enter 0 if you are not connected to the target AP: ")
 if (int(mode) == 1):
-    intHope = search.binarySearch(tracker, "pan")
-    intPray = tracker.get_strength_connected()
-    print("%d db at %d degrees" % (intPray, intHope))
+#    intHope = search.Search1(tracker, "pan")
+#    intPray = tracker.get_strength_connected()
+#    print("%d db at %d degrees" % (intPray, intHope))
+
+    intHope = search.Search2(tracker)
+    print(tracker)
 else:
     tracker.select_target()
+    
