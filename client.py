37387ba956286de70e9d6389d4c7629b740d35d6
Magikarp <magikarp@karp.karp>
2014-04-15 21:50:48 -0400

    removed printing of server response and unneeded break

--- client.py
+++ client.py
@@ -25,9 +25,6 @@ class socks(object):
                 data = sock.recvfrom(1024)
                 reply = data[0]
                 addr = data[1]
-                print("server: " + reply)
-                if reply=="dead":
-                    break
                 time.sleep(5)
             except:
                 print("send or recv failed, please check server if send succeeded\
