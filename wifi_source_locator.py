a68e50fb7729506b67165a25929e9ffab738576e
Magikarp <magikarp@karp.karp>
2014-04-15 23:39:50 -0400

    testing sending node data to server

--- wifi_source_locator.py
+++ wifi_source_locator.py
@@ -8,6 +8,13 @@ import client
 strInterface = "wlp1s0"
 tracker_1 = node.Node(strInterface)
 tracker_2 = node.Node(strInterface)
+
+com_1 = client.socks()
+com_2 = client.socks()
+
+host_ip = "192.168.1.24"
+host_port = 6869
+
 mode = input("Please enter 1 if you are connected to the target AP, otherwise\n\
         enter 0 if you are not connected to the target AP: ")
 if (int(mode) == 1):
@@ -17,6 +24,14 @@ if (int(mode) == 1):
     search.Search1(tracker_1, "pan", connected)
     search.Search1(tracker_1, "tilt", connected)
     search.Search2(tracker_2, connected)
+    tracker_1_data = str(tracker_1)
+    tracker_2_data = str(tracker_2)
+    print("sending tracker_1")
+    com_1.sendNodeData(host_ip, host_port, tracker_1_data.encode())
+    print("sent tracker_1")
+    print("sending tracker_2")
+    com_2.sendNodeData(host_ip, host_port, tracker_2_data.encode())
+    print("sent tracker_2")
 else:
     
     connected = False
