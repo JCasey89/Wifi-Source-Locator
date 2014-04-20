1e50d353bb88f2064d410e0d5922c523a31eb51d
Magikarp <magikarp@karp.karp>
2014-04-20 00:55:28 -0400

    more major work on getting this rolling

--- triangle.py
+++ triangle.py
@@ -3,52 +3,58 @@ import math
 import node
 import sys
 
-def distance(node1, node2):
-    doubleDistance = 0
+def distance(a, b):
+    
+    c = math.sqrt(a*a + b*b)
 
-    return doubleDistance
+    return c
 
     #       C
     #    b    a
     #   A   c  B
     # C = 180 - A - B
-    # b = (c/sinC)*sinB
-    # a = (c/sinC)*sinB 
-
+    # a = (c/sinC)*sinA 
+    # node2 = A
+    # node1 = B
+    
 def triangulate(node1, node2):
     boolNode1Closer = False
-    node1Long = 0
-    node1Lat = 0
-    node2Long = 0
-    node2Lat = 0
-
-
-    distNode2Node = distance(node1, node2)
-
-    intTgtAngle = 180 - node1.intServoPanDegree - node2.intServoPanDegree
+    node1Long = node1.floatLongitude
+    node1Lat = node1.floatLatitude
+    node2Long = node2.floatLongitude
+    node2Lat = node2.floatLatitude
     
-    if (node1.intWifiStrength > node2.intWifiStrength):
-        distNode2Target = calcDistNode2Target(node1, distNode2Node,\
-                intTgtAngle)
-        boolNode1Closer = True
-    elif (node1.intWifiStrength < node2.intWifiStrength or \
-            node1.intWifiStrength == node2.intWifiStrength):
-        boolNode1Closer = False
-        distNode2Target = calcDistNode2Target(node2, distNode2Node,\
-                intTgtAngle)
+    angle_B = math.radians(node1.intServoPanDegree) - \
+            math.atan(abs(node1Lat - node2Lat)/abs(node1Long - node2Long))
+    
+    angle_A = math.radians(180 - node2.intServoPanDegree) - \
+            math.atan(abs(node1Lat - node2Lat)/abs(node1Long - node2Long))
+
+    side_c = distance(node1Lat - node2Lat, node1Long - node2Long)
+
+    angle_C = math.radians(180 - math.degrees(node1_angle_rad) -\
+            math.degrees(node2_angle_rad))
+    
+    side_a = (side_c/math.sin(angle_C))*math.sin(angle_A)
+    side_b = (side_c/math.sin(angle_C))*math.sin(angle_B)
+
+    #from node 1(B)
+    longDif = math.cos(angle_B)*side_a
+    latDif = math.sin(angle_B)*side_a
+    
+    if (node1.intServoPanDegree < 90) :
+        finalLong = node1Long - longDif
+        finalLat = node1Lat + latDif
+    elif(node1.intServoPanDegree > 90):
+        finalLong = node1Long + longDif
+        finalLat = node1Lat + latDif
+    elif(node1.intServoPanDegree == 90):
+        finalLong = node1Long
+        finalLat = node1Lat + latDif
     else:
-        print("math error in triangulation during law of sines")
+        print("error during trianglation")
         sys.exit(1)
-
-    if boolNode1Closer:
-        finalLat = math.asin(math.sin(node1Lat)
-    elif not boolNode1Closer:
-        pass
-    else:
-        print("Error during loc calc via bearing and dist from node to tgt")
-        sys.exit(1)
-
-
-#    targetCoord(finalLong, finalLat)
+    
+    targetCoord = (finalLong, finalLat)
 
     return targetCoord
