3a63781923e4100ed786fa4dc6c7a23d480a62c3
GH0S1_R33P0R <gh0s1@jubatus>
2014-04-21 00:16:04 -0400

    Updated to work with new Arduino Code

--- ArduinoCode/pySerialTest.py
+++ ArduinoCode/pySerialTest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import serial
+import time
 ser = serial.Serial(port='/dev/ttyACM1',
         baudrate=115200) #Check this if fail
 print(ser.name)
@@ -8,36 +9,44 @@ print(ser.name)
 degree = "0";
 
 
-def writeToA(inDegree):
-    print("Writing to servo A")
+def write_to_pan(inDegree):
     degree = "A" + str(inDegree) + "\n"
     ser.write(degree.encode('utf-8'))
     ser.flush()
-    print("done")
+    time.sleep(1)
     return
 
 
-def isFix():
-    print("Checking if there is a fix")
+def is_fixed():
     message = "B\n"
     ser.write(message.encode('utf-8'))
     ser.flush()
-    print(str(ser.readline()))
-    return
+    returned = ser.readline()
 
-def getLocation():
-    print("Getting Location")
+    if(returned ==  b'0\r\n'):
+        return False
+    else:
+        return True
+
+def get_location():
     message = "C\n"
     ser.write(message.encode('utf-8'))
     ser.flush()
-    print(str(ser.readline()))
-    return
+    return str(ser.readline().decode("utf-8").rstrip())
 
 
 while(True):
     degree = int(input()) 
     print("You entered" + str(degree))
-    writeToA(degree)
-    isFix()
-    getLocation();
+
+    print("Writing to servo A")
+    write_to_pan(degree);
+    print("Done!\n")
+    print("Checking if there is a fix")
+    if(is_fixed()):
+        print("\tGot a fix WOO!!!")
+    else:
+        print("\tNo fix, boo :( ")
+        
+    print("Getting Location:[" + get_location() + "]")
 
